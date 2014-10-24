975f6d3b333a207a61a4d06446413a077812364c
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-10-24 12:12:53 +0100

    AW - Created the CRUD sprocs for the user

--- Advanced Web/Assignment/Database Schema.sql
+++ Advanced Web/Assignment/Database Schema.sql
@@ -36,3 +36,102 @@ CREATE TABLE IF NOT EXISTS shift_table (
 	FOREIGN KEY (userID) REFERENCES user_table (userID)
 );
 
+-- Create the Stored Procedures
+-- Delimter changes the ending to // instead of ; Allowing ; to be passed in the sproc
+
+DROP PROCEDURE IF EXISTS user_get;
+DELIMITER //
+CREATE PROCEDURE user_get
+(
+	IN id int
+	)
+BEGIN
+	SELECT 	u.surname,
+			u.forename,
+			l.levelName,
+			u.levelID,
+			u.staffID
+	FROM	user_table AS u
+			INNER JOIN level_table AS l
+	WHERE	u.userID = id;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS user_getAll;
+DELIMITER //
+CREATE PROCEDURE user_getAll()
+BEGIN
+	SELECT 	u.surname,
+			u.forename,
+			l.levelName,
+			u.levelID,
+			u.staffID
+	FROM	user_table AS u
+			INNER JOIN level_table AS l;
+END //
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS user_add;
+DELIMITER //
+CREATE PROCEDURE user_add
+(
+	IN surnameIN varchar(200),
+	IN forenameIN varchar(200),
+	IN passwordIN varchar(100),
+	IN levelIDIN int,
+	IN staffIDIN int
+)
+BEGIN
+	INSERT INTO user_table (
+				surname,
+				forename,
+				password,
+				levelID,
+				staffID
+		)
+	VALUES	(
+				surnameIN,
+				forenameIN,
+				passwordIN,
+				levelIDIN,
+				staffIDIN
+		);
+END //
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS user_delete;
+DELIMITER //
+CREATE PROCEDURE user_delete
+(
+	IN ID int
+)
+BEGIN
+	DELETE FROM user_table
+	WHERE	userID = id;
+END //
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS user_edit;
+DELIMITER //
+CREATE PROCEDURE user_edit
+(
+	IN id int,
+	IN surnameIN varchar(200),
+	IN forenameIN varchar(200),
+	IN passwordIN varchar(100),
+	IN levelIDIN int,
+	IN staffIDIN int
+)
+BEGIN
+	UPDATE user_table
+	SET 	surname = surnameIN,
+			forname = forenameIN,
+			password = passwordIN,
+			levelID = levelIDIN,
+			staffID = staffIDIN
+	WHERE userID = id;
+END //
+DELIMITER ;
