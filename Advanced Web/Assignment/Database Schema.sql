aa628ed113cf0f49f8c9e70b4c75b730bcd01476
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-10-24 11:43:56 +0100

    AW - Added in some more design, and justification for the table designs

--- Advanced Web/Assignment/Database Schema.sql
+++ Advanced Web/Assignment/Database Schema.sql
@@ -1,12 +1,12 @@
 
 -- Drop the tables if they are already in the DB
-DROP TABLE IF EXISTS `shift_table`;
-DROP TABLE IF EXISTS `user_table`;
-DROP TABLE IF EXISTS `level_table`;
+DROP TABLE IF EXISTS shift_table;
+DROP TABLE IF EXISTS user_table;
+DROP TABLE IF EXISTS level_table;
 
 
---Create the new tables
-CREATE TABLE IF NOT EXISTS `level_table` (
+-- Create the new tables
+CREATE TABLE IF NOT EXISTS level_table (
 	levelID int NOT NULL AUTO_INCREMENT,
 	levelName varchar(100) COLLATE utf8_unicode_ci NOT NULL,
 	PRIMARY KEY (levelID),
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `level_table` (
 );
 
 
-CREATE TABLE IF NOT EXISTS `user_table` (
+CREATE TABLE IF NOT EXISTS user_table (
 	userID int NOT NULL AUTO_INCREMENT,
 	surname varchar(200) COLLATE utf8_unicode_ci NOT NULL,
 	forename varchar(200) COLLATE utf8_unicode_ci NOT NULL,
@@ -27,8 +27,8 @@ CREATE TABLE IF NOT EXISTS `user_table` (
 );
 
 
-CREATE TABLE IF NOT EXISTS `shift_table` (
-	shiftID int NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS shift_table (
+	shiftID bigint NOT NULL AUTO_INCREMENT,
 	userID int NOT NULL,
 	shiftDate date NOT NULL,
 	PRIMARY KEY (shiftID),
