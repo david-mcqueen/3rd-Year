888139e8969953de6796d0427d4a3f801b53fc6a
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-10-24 12:52:17 +0100

    AW - Added in some LEVEL and SHIFT spruces. Shift sprocs will need more work depending on what data needs returning where & when -- Also added in some data

--- Advanced Web/Assignment/Database Schema.sql
+++ Advanced Web/Assignment/Database Schema.sql
@@ -36,6 +36,9 @@ CREATE TABLE IF NOT EXISTS shift_table (
 	FOREIGN KEY (userID) REFERENCES user_table (userID)
 );
 
+
+
+
 -- Create the Stored Procedures
 -- Delimter changes the ending to // instead of ; Allowing ; to be passed in the sproc
 
@@ -44,7 +47,7 @@ DELIMITER //
 CREATE PROCEDURE user_get
 (
 	IN id int
-	)
+)
 BEGIN
 	SELECT 	u.surname,
 			u.forename,
@@ -61,7 +64,8 @@ DROP PROCEDURE IF EXISTS user_getAll;
 DELIMITER //
 CREATE PROCEDURE user_getAll()
 BEGIN
-	SELECT 	u.surname,
+	SELECT 	u.userID,
+			u.surname,
 			u.forename,
 			l.levelName,
 			u.levelID,
@@ -135,3 +139,136 @@ BEGIN
 	WHERE userID = id;
 END //
 DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS level_add;
+DELIMITER //
+CREATE PROCEDURE level_add
+(
+	levelNameIN varchar(100)
+)
+BEGIN
+	INSERT INTO level_table(
+			levelName
+		)
+	VALUES (
+			levelNameIN
+		);
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS level_edit;
+DELIMITER //
+CREATE PROCEDURE level_edit
+(
+	IN levelIDIN int,
+	IN levelNameIN varchar(100)
+)
+BEGIN
+	UPDATE 	level_table
+	SET 	levelName = levelNameIN
+	WHERE	levelID = levelIDIN;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS level_delete;
+DELIMITER //
+CREATE PROCEDURE level_delete
+(
+	IN levelIDIN int
+)
+BEGIN
+	DELETE FROM level_table
+	WHERE	levelID = levelIDIN;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS level_get;
+DELIMITER //
+CREATE PROCEDURE level_get
+(
+	IN levelIDIN int
+)
+BEGIN
+	SELECT 	levelName
+	FROM	level_table
+	WHERE	levelID = levelIDIN;
+END
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS level_getAll;
+DELIMITER //
+CREATE PROCEDURE level_getAll()
+BEGIN
+	SELECT 	levelID,
+			levelName
+	FROM	level_table;
+END //
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS shift_add;
+DELIMITER //
+CREATE PROCEDURE shift_add
+(
+	IN userIDIN int,
+	IN shiftDateIN date
+)
+BEGIN
+	INSERT INTO shift_table (
+				userID,
+				shiftDate
+		)
+	VALUES (
+				userIDIN,
+				shiftDateIN
+		);
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS shift_delete;
+DELIMITER //
+CREATE PROCEDURE shift_delete
+(
+	IN shiftIDIN int
+)
+BEGIN
+	DELETE FROM shift_table
+	WHERE	shiftID = shiftIDIN;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS shift_edit;
+DELIMITER //
+CREATE PROCEDURE shift_edit
+(
+	IN shiftIDIN int,
+	IN userIDIN int,
+	IN shiftDateIN int
+)
+BEGIN
+	UPDATE 	shift_table
+	SET 	userID = userIDIN,
+			shiftDate = shiftDateIN
+	WHERE	shiftID = shiftIDIN;
+END //
+DELIMITER ;
+
+
+
+-- Add some data
+call level_add('admin');
+call level_add('Standard');
+call level_add('Senior');
+
+call user_add ('Apple', 'Amy', 'Ppl!eta123', 2, 4567);
+call user_add ('Berry', 'Bert', 'aSerty456a', 2, 5467);
+call user_add ('Carrot', 'Carl', 'asDghj1', 2, 1432);
+call user_add ('Donkey', 'Dave', 'sgGgdghj1', 2, 6743);
+call user_add ('Emu', 'Ernie', 'tyuIo124', 2, 2456);
+call user_add ('Fur', 'Frank', '45frAnk67', 3, 8543);
+call user_add ('Goat', 'Graham', 'deDede1', 3, 7832);
+call user_add ('timetabler', 'admin', 'organ1sed', 1, 6189);
+
+
+
