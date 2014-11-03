84e1b421cae5ee1d4bfe5a2016771900e28d9e26
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-11-03 21:06:41 +0000

    AW - Validation to check if the user is already working for the clicked day.

--- Advanced Web/Assignment/Production/Assignment/application/views/user/calendar.php
+++ Advanced Web/Assignment/Production/Assignment/application/views/user/calendar.php
@@ -13,105 +13,109 @@ echo '<h1>Welcome, ' .  $forename .' ' . $surname . '</h1>';
 <script src='<?php echo base_url(); ?>application/third_party/fullcalendar-2.1.1/fullcalendar.min.js'></script>
 <script>
 
-        $(document).ready(function() {
+$(document).ready(function() {
 
-            var maxDate = new Date(),
-                deleteMessage;
-            maxDate.setMonth(maxDate.getMonth() + 3); //Limit of 3 months in the future
-            $('#calendar').fullCalendar({
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,basicWeek'
-                },
-                firstDay:1, //Week starts on Monday
-                defaultDate: Date(),
-                weekNumbers: true,
-                editable: true, //So users can click to add shifts
-                eventLimit: true, // allow "more" link when too many events
-                events: {
-                    url: '<?php echo base_url(); ?>index.php/shift/getCalendar',
-                    error: function(textStatus, errorThrown) {
-                        $('#script-warning').show();
-                        alert(errorThrown.responseText);
-                        alert(textStatus);
-                    }
-                },
-                fixedWeekCount: false,
-                loading: function(bool) {
-                    $('#loading').toggle(bool);
-                },
-                viewRender: function(view){
-                    //Stops the user going more than 3 months in the future.
-                    if (view.start > maxDate){
-                        $('#calendar').fullCalendar('gotoDate', maxDate);
-                        alert("Only 3 months forecast is available");
-                    }
-                },
-                dayRender: function(date, cell){
-                    //Disables cells that are out of range
-                    if (date > maxDate){
-                        $(cell).addClass('disabled');
-                    }
-                },
-                eventClick: function(calEvent, jsEvent, view) {
-                    if (calEvent.onShift == 1){
-                        //Only process deletion if the user is on this shift
+    var maxDate = new Date(),
+        deleteMessage,
+        dayEvents,
+        onShift;
 
-                        deleteMessage = "Are you sure you want to remove your shift from date " + calEvent.shiftDate + "?";
-                        if(confirm (deleteMessage)){
-                            $.ajax({
-                                url: "<?php echo base_url(); ?>index.php/shift/removeShift",
-                                dataType: 'json',
-                                data: {
-                                    id: calEvent.id
-                                },
-                                success: function (result) {
-                                    if (!result[0].success){
-                                        alert(result[0].errors);
-                                    }else{
-                                        $('#calendar').fullCalendar('refetchEvents');
-                                    }
-                                },
-                                error: function () {
-                                    alert("Oops! Something didn't work");
-                                }
-                            });
-                        }
-                    }
+    maxDate.setMonth(maxDate.getMonth() + 3); //Limit of 3 months in the future
 
-                },
-                dayClick: function(date, jsEvent, view) {
-                    //Will be used to add shifts
+    $('#calendar').fullCalendar({
+        header: { //Define the buttons
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month'
+        },
+        firstDay:1, //Week starts on Monday
+        defaultDate: Date(),
+        weekNumbers: true,
+        editable: true, //So users can click to add shifts
+        eventLimit: true, // allow "more" link when too many events
+        events: {
+            url: '<?php echo base_url(); ?>index.php/shift/getCalendar',
+            error: function(textStatus, errorThrown) {
+                alert(errorThrown.responseText);
+                alert(textStatus);
+            }
+        },
+        fixedWeekCount: false,
+        loading: function(bool) {
+            $('#loading').toggle(bool);
+        },
+        viewRender: function(view){
+            //Stops the user going more than 3 months in the future.
+            if (view.start > maxDate){
+                $('#calendar').fullCalendar('gotoDate', maxDate);
+                alert("Only 3 months forecast is available");
+            }
+        },
+        dayRender: function(date, cell){
+            //Disables cells that are out of range
+            if (date > maxDate){
+                $(cell).addClass('disabled');
+            }
+        },
+        eventClick: function(calEvent, jsEvent, view) {
+            if (calEvent.onShift == 1){
+                //Only process deletion if the user is on this shift
+
+                deleteMessage = "Are you sure you want to remove your shift from date " + calEvent.shiftDate + "?";
+                if(confirm (deleteMessage)){
                     $.ajax({
-                        url: "<?php echo base_url(); ?>index.php/shift/addShift",
+                        url: "<?php echo base_url(); ?>index.php/shift/removeShift",
                         dataType: 'json',
                         data: {
-                            title: '',
-                            start: date.format()
+                            id: calEvent.id
                         },
                         success: function (result) {
-                            console.log(result[0].title);
-//                            $('#calendar').fullCalendar('renderEvent',
-//                                {
-//                                    title: result[0].title,
-//                                    start: date
-//                                }, true);
-                            $('#calendar').fullCalendar('refetchEvents');
+                            if (!result[0].success){
+                                alert(result[0].errors);
+                            }else{
+                                $('#calendar').fullCalendar('refetchEvents');
+                            }
                         },
                         error: function () {
                             alert("Oops! Something didn't work");
                         }
                     });
+                }
+            }
 
-
+        },
+        dayClick: function(date, jsEvent, view) {
+            dayEvents = $('#calendar').fullCalendar( 'clientEvents' ,function(event){
+                return moment(event.start).isSame(date, 'day'); //Return all events for the clicked day
+            });
+            onShift = false;
+            dayEvents.forEach(function(entry){
+                //For each of the calendar events on clicked day, determine if the user is already working
+                if (entry.onShift == 1){
+                    onShift = true;
                 }
             });
-
-
-        });
-
-
+            if (!onShift){
+                //If the user is not already working. Attempt to add a new shift
+                $.ajax({
+                    url: "<?php echo base_url(); ?>index.php/shift/addShift",
+                    dataType: 'json',
+                    data: {
+                        title: '',
+                        start: date.format()
+                    },
+                    success: function (result) {
+                        console.log(result[0].title);
+                        $('#calendar').fullCalendar('refetchEvents');
+                    },
+                    error: function () {
+                        alert("Oops! Something didn't work");
+                    }
+                });
+            }
+        }
+    });
+});
 </script>
 <style>
 
