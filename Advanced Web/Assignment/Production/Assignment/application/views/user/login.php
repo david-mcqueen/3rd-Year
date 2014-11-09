9c1598e9853157c97514936ae5d7c25f68b0c288
DavidMcQueen <dave.j.mcqueen@gmail.com>
2014-11-09 11:19:57 +0000

    AW - Modified the login page to pattern match before the form is submitted

--- Advanced Web/Assignment/Production/Assignment/application/views/user/login.php
+++ Advanced Web/Assignment/Production/Assignment/application/views/user/login.php
@@ -13,10 +13,37 @@ Home Page - Login
 <?php echo validation_errors(); ?>
 <?php echo form_open('index.php/VerifyLogin'); ?>
 <label for="username">Username:</label>
-<input type="text" size="20" id="username" name="username"/>
+<input type="text"
+       size="20"
+       id="username"
+       name="username"
+       required
+       pattern="^([0-9]{4}|[a-zA-Z]\.[a-zA-Z]+)@nhs\.org$"
+    />
+
 <br/>
 <label for="password">Password:</label>
-<input type="password" size="20" id="passowrd" name="password"/>
+<input type="password"
+       size="20"
+       id="passowrd"
+       name="password"
+       required
+    />
 <br/>
-<input type="submit" value="Login"/>
+<input type="submit"
+       value="Login"
+    />
 </form>
+<?php
+/*
+Regex Explanation:
+    ^([0-9]{4}|[a-zA-Z]\.[a-zA-Z]+)@nhs\.org$
+
+    ^                       Start of string
+    [0-9]{4}                Contain 4 digits
+    |                       OR
+    [a-zA-Z]\.[a-zA-Z]+     Single character . string of characters
+    @stu\.mmu\.ac\.uk$      End with the email address
+
+*/
+?>
